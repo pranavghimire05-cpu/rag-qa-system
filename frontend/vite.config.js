fa4db890f13cc17d4b2b2e20523725e1214c492a
pranavghimire05-cpu <pranavghimire05@gmail.com>
2026-07-19 11:53:07 +0545

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://rag-qa-system-9hbg.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
